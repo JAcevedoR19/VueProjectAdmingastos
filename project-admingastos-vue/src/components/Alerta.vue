b61cc426171f731ef4941e5106bb8aeaa3c0dc76
JAcevedoR19 <joanaar19@gmail.com>
2024-07-17 18:51:24 -0300

    Se crearon los componentes de Presupuesto y Alerta, y una pequeña validación para presupuestos por debajo de 0

---
+++ project-admingastos-vue/src/components/Alerta.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="alerta">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .alerta {
+        padding: 1rem;
+        font-weight: 900;
+        text-transform: uppercase;
+        font-size: 1.8rem;
+        background-color: var(--color-white);
+        color: var(--color-red);
+        border-left: .5rem solid var(--color-red);
+        margin-bottom: 2rem;
+        text-align: center;
+    }
+</style>
---
+++ project-admingastos-vue/src/components/Presupuesto.vue
@@ -0,0 +1,81 @@
+<script setup>
+    import { ref } from 'vue';
+    import Alerta from './Alerta.vue';
+
+    const presupuesto = ref(0);
+    const error = ref('');
+
+    const definirPresupuesto = () => {
+
+        if(presupuesto.value <= 0) {
+            error.value = 'El presupuesto debe ser mayor a 0';
+
+            setTimeout(() => {
+                error.value = '';
+            }, 3000);
+        }
+
+
+    }
+</script>
+
+<template>
+    <form action="" class="presupuesto" 
+        @submit.prevent="definirPresupuesto"
+    >
+        <Alerta v-if="error"> 
+            {{ error }}
+        </Alerta>
+        <div class="campo">
+            <label for="nuevo-presupuesto">Definir Presupuesto</label>
+            <input  type="number" id="nuevo-presupuesto" 
+                class="nuevo-presupuesto" placeholder="Añade tu presupuesto"
+                min="0"
+                v-model.number="presupuesto"
+            >
+        </div>
+        <input type="submit" value="Confirmar Presupuesto">
+
+    </form>
+</template>
+
+<style scoped>
+    .presupuesto {
+        width: 100%;
+    }
+    .campo {
+        display: grid;
+        gap: 2rem;
+    }
+    .presupuesto label {
+        font-size: 2.5rem;
+        text-align: center;
+        color: var(--color-blue);
+        font-weight: 900;
+    }
+    .presupuesto input[type="number"] {
+        background-color: var(--color-light-gray);
+        border: none;
+        padding: 1.5rem;
+        text-align: center;
+        border-radius: 1rem;
+        font-size: 2.2rem;
+    }
+    .presupuesto input[type="submit"] {
+        background-color: var(--color-blue);
+        border: none;
+        padding: 1rem;
+        text-align: center;
+        border-radius: 1rem;
+        font-size: 2rem;
+        font-weight: 900;
+        color: var(--color-white);
+        width: 100%;
+        margin-top: 2rem;
+        transition: background-color .3s ease;
+    }
+    .presupuesto input[type="submit"]:hover {
+        cursor: pointer;
+        background-color: var(--color-dark-blue);
+    }
+</style>
